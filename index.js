41e53c4dae403eba750c26b44e26bc34411c9356
Thomas England <tom@england.dev>
2025-03-09 08:46:18 -1000

    Initial commit

---
+++ index.js
@@ -0,0 +1,51 @@
+let homeScore = 0
+let guestScore = 0
+let homeScoreLabel = document.getElementById("homeScore")
+let guestScoreLabel = document.getElementById("guestScore")
+
+function plusOne() {
+  const clickedElement = event.target;
+  const parentElement = clickedElement.parentNode;
+  if (parentElement.id == "home") {
+        homeScore++
+        homeScoreLabel.textContent = homeScore
+        
+  } else {
+        guestScore++
+        guestScoreLabel.textContent = guestScore
+  }
+}
+
+function plusTwo() {
+    const clickedElement = event.target;
+    const parentElement = clickedElement.parentNode;
+    if (parentElement.id == "home") {
+            homeScore += 2
+            homeScoreLabel.textContent = homeScore
+        
+    } else {
+            guestScore += 2
+            guestScoreLabel.textContent = guestScore
+    }
+    
+}
+
+function plusThree() {
+    const clickedElement = event.target;
+    const parentElement = clickedElement.parentNode;
+    if (parentElement.id == "home") {
+            homeScore += 3
+            homeScoreLabel.textContent = homeScore
+        
+    } else {
+            guestScore += 3
+            guestScoreLabel.textContent = guestScore
+    }   
+}
+
+function reset() {
+    guestScore = 0
+    homeScore = 0
+    guestScoreLabel.textContent = guestScore
+    homeScoreLabel.textContent = homeScore
+}
