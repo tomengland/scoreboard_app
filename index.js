c3aff2d0e4865ab7c1ddd8f30d6e77cfda229c08
Thomas England <tom@england.dev>
2025-03-09 10:03:31 -1000

    fix kyle bug

--- index.js
+++ index.js
@@ -2,14 +2,16 @@ let homeScore = 0
 let guestScore = 0
 let homeScoreLabel = document.getElementById("homeScore")
 let guestScoreLabel = document.getElementById("guestScore")
-
+function isMax() {
+        return homeScore >= 997 || guestScore >= 997
+}
 function plusOne() {
   const clickedElement = event.target;
   const parentElement = clickedElement.parentNode;
-  if (parentElement.id == "home") {
+  if (parentElement.id == "home" && !isMax()) {
         homeScore++
         homeScoreLabel.textContent = homeScore
-        
+
   } else {
         guestScore++
         guestScoreLabel.textContent = guestScore
@@ -19,28 +21,28 @@ function plusOne() {
 function plusTwo() {
     const clickedElement = event.target;
     const parentElement = clickedElement.parentNode;
-    if (parentElement.id == "home") {
+    if (parentElement.id == "home" && !isMax()) {
             homeScore += 2
             homeScoreLabel.textContent = homeScore
-        
+
     } else {
             guestScore += 2
             guestScoreLabel.textContent = guestScore
     }
-    
+
 }
 
 function plusThree() {
     const clickedElement = event.target;
     const parentElement = clickedElement.parentNode;
-    if (parentElement.id == "home") {
+    if (parentElement.id == "home" && !isMax()) {
             homeScore += 3
             homeScoreLabel.textContent = homeScore
-        
+
     } else {
             guestScore += 3
             guestScoreLabel.textContent = guestScore
-    }   
+    }
 }
 
 function reset() {
